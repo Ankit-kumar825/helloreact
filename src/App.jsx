a50e3e8fb3568f59df8cc947c2b25ba819404683
Ankit-kumar825 <ankitk60695@gmail.com>
2026-02-03 10:44:59 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="app">
+      <div className="content">
+        <div className="title">Hello World</div>
+        <div className="name">I am Ankit Kumar</div>
+
+        <div className="details">
+          <p>Role: React Developer</p>
+          <p>Skills: HTML, CSS, JavaScript, React</p>
+          <p>Learning: Frontend Development</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
